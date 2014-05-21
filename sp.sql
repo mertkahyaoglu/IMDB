da3dbda8d4563adde464d94b060af447ff1696f6
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2014-05-21 23:52:03 +0300

    Update sp.sql

--- sp.sql
+++ sp.sql
@@ -1,6 +1,3 @@
--- --------------------------------------------------------------------------------
--- getGenres
--- --------------------------------------------------------------------------------
 DELIMITER $$
 
 CREATE PROCEDURE `getGenres`(IN iid CHAR(9))
@@ -11,21 +8,21 @@ END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getActors`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMovieActors`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT name FROM actors, movies, movie_cast WHERE movies.id = movie_cast.movie_id and movie_cast.actor_id = actors.id and movies.imdbID = imdb_ID;
 END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getDirectors`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMovieDirectors`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT name FROM directors, movies, movie_director WHERE directors.id = movie_director.director_id and movie_director.movie_id = movies.id and movies.imdbID = imdb_ID;
 END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getPlot`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMoviePlot`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT plot FROM plots, movies WHERE plots.movie_id = id and movies.imdbID = imdb_ID;
 
@@ -33,21 +30,21 @@ END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getPoster`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMoviePoster`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT url FROM posters, movies WHERE posters.movie_id = movies.id and movies.imdbID = imdb_ID  ;
 END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getRating`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMovieRating`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT imdb_rating FROM stats,movies WHERE stats.movie_id = movies.id and movies.imdbID = imdb_ID;
 END
 
 DELIMITER $$
 
-CREATE PROCEDURE `getWriters`(IN imdb_ID VARCHAR(9))
+CREATE PROCEDURE `getMovieWriters`(IN imdb_ID VARCHAR(9))
 BEGIN
 	SELECT name FROM writers, movie_writer, movies WHERE movies.id = movie_writer.movie_id and movie_writer.writer_id=writers.id and movies.imdbID = imdb_ID;
 END
