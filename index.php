95d484cbbf64bc6e4a5b4aa99202f821e8f93008
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2014-06-05 22:40:59 +0300

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php require('templates/header.php'); require('core/init.php');
-if(getNumOfMovies() < 1) load(); load();
+if(getNumOfMovies() < 1) load();
 $recents = getRecent(20);
 ?>
     <div class="slider">
